3602d7d85ec35a64855e459e4009a4aaf5d2929a
Ewout Merckx <ewoutmerckx.pro@gmail.com>
2016-05-12 14:19:16 +0200

    added exercise 12

--- labs/ex_12/ex_12.cpp
+++ labs/ex_12/ex_12.cpp
@@ -18,9 +18,9 @@ int main( int argc, char** argv )
 
     // read the image
     Mat image1;
-    image1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    image1 = imread( argv[1], CV_LOAD_IMAGE_COLOR );
     Mat image2;
-    image2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+    image2 = imread( argv[2], CV_LOAD_IMAGE_COLOR );
 
     // check for errors
     if(! image1.data )
@@ -36,8 +36,32 @@ int main( int argc, char** argv )
 
     //-----------------------------------------------------------------------------------
 
-    // STEP 1: 
+    // STEP 1: convert images to grayscale for better detection
+    Mat gray1;
+    cvtColor( image1, gray1, CV_RGB2GRAY );
+    Mat gray2;
+    cvtColor( image2, gray2, CV_RGB2GRAY );
 
+    // STEP 2: get the Harris corners
+    // void goodFeaturesToTrack(InputArray image, OutputArray corners, int maxCorners, 
+    //                          double qualityLevel, double minDistance, InputArray mask=noArray(),
+    //                          int blockSize=3, bool useHarrisDetector=false, double k=0.04 )
+    vector<Point2f> harrisCorners1;
+    goodFeaturesToTrack(gray1, harrisCorners1, 200, 0.05, 10, Mat(), 3, true, 0.04);
+    vector<Point2f> harrisCorners2;
+    goodFeaturesToTrack(gray2, harrisCorners2, 200, 0.05, 10, Mat(), 3, true, 0.04);
+
+    // STEP 3: show the corners in the image via green circles
+    // void circle(Mat& img, Point center, int radius, const Scalar& color, int thickness=1, int lineType=8)
+    int radius = 2;
+    Mat result1 = image1.clone();
+    for( int i = 0; i < harrisCorners1.size(); i++ ){ 
+        circle( result1, harrisCorners1[i], radius, Scalar(0,255,0), -1, 8 ); 
+    }
+    Mat result2 = image2.clone();
+    for( int i = 0; i < harrisCorners1.size(); i++ ){ 
+        circle( result2, harrisCorners2[i], radius, Scalar(0,255,0), -1, 8 ); 
+    }
 
     //-----------------------------------------------------------------------------------
 
@@ -45,26 +69,25 @@ int main( int argc, char** argv )
     string windowName1 = "Original image 1";
     namedWindow( windowName1, WINDOW_AUTOSIZE );
     imshow( windowName1, image1 );
-    moveWindow( windowName1, 0, 0);
+    moveWindow( windowName1, 0, 0 );
 
-    // show the original image
+    // show the original image 2
     string windowName2 = "Original image 2";
     namedWindow( windowName2, WINDOW_AUTOSIZE );
     imshow( windowName2, image2 );
-    moveWindow( windowName2, image1.size().width, 0);
+    moveWindow( windowName2, image1.size().width, 0 );
 
-    /*// show the canny edges
-    string windowName2 = "Canny edges";
-    namedWindow( windowName2, WINDOW_AUTOSIZE );
-    imshow( windowName2, cannyEdges );
-    moveWindow( windowName2, image.size().width, 0);
-
-    // show the original image
-    string windowName3 = "Result";
+    // show the result image 1
+    string windowName3 = "Result image 1";
     namedWindow( windowName3, WINDOW_AUTOSIZE );
-    imshow( windowName3, image_dst );
-    moveWindow( windowName3, 
-        image.size().width + cannyEdges.size().width, 0);*/
+    imshow( windowName3, result1 );
+    moveWindow( windowName3, 0, image1.size().height + 55 );
+
+    // show the result image 1
+    string windowName4 = "Result image 2";
+    namedWindow( windowName4, WINDOW_AUTOSIZE );
+    imshow( windowName4, result2 );
+    moveWindow( windowName4, image1.size().width, image1.size().height + 55 );
 
     //-----------------------------------------------------------------------------------
 
